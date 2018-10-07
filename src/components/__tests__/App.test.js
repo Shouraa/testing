7be1c3508ddb825a3c60600ae23ad29b76cd5307
Shouraa <shorajafar@gmail.com>
2018-10-07 21:34:02 +0300

    the save comment action creator

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "../App";
+import { shallow } from 'enzyme';
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from 'components/CommentList';
+
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+
+  
+});
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
 
-  ReactDOM.render(<App />, div);
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  expect(div.innerHTML).toContain("Comment Box");
-  ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', () => {
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });

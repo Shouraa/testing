9a294ec59bcd8cfdd3688c9f3cb10797500e3dea
Shora <shorajafar@gmail.com>
2018-10-08 15:44:42 +0300

    43 handling unknown types

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
-  state = { comment: '' };
+  state = { comment: "" };
 
   handleChange = event => {
-    this.setState({ comment: event.target.value })
+    this.setState({ comment: event.target.value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
 
-    // TODO - Call an action creator
-    // And save the comment
-    
+    this.props.saveComment(this.state.comment);
 
-    this.setState({ comment: ''});
-  }
+    this.setState({ comment: "" });
+  };
 
   render() {
     return (
@@ -30,4 +30,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);

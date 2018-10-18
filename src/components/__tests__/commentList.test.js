b05014a5bf5e2b605cca9364cecd1a826055a39c
Shora <shorajafar@gmail.com>
2018-10-18 17:05:03 +0300

    testing

--- src/components/__tests__/commentList.test.js
+++ src/components/__tests__/commentList.test.js
@@ -8,7 +8,7 @@ let wrapped;
 
 beforeEach(() => {
   const initialState = {
-    comments: ['Comment 1', 'Comment 2']
+    comments: ["Comment 1", "Comment 2"]
   };
   wrapped = mount(
     <Root initialState={initialState}>
@@ -18,5 +18,10 @@ beforeEach(() => {
 });
 
 it("creates on LI per comment", () => {
-  console.log(wrapped.find('li').length);
+  expect(wrapped.find("li").length).toEqual(2);
+});
+
+it("shows the text for each comment", () => {
+  expect(wrapped.render().text()).toContain("Comment 1");
+  expect(wrapped.render().text()).toContain("Comment 2");
 });
